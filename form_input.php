c3d0bedd4b5bd68ce5c356056b8e22561a28df48
Akania Isabela <akaniaisabela1@gmail.com>
2024-10-30 17:29:03 +0700

    first commit

---
+++ form_input.php
@@ -0,0 +1,6 @@
+<form action="proses_input.php" method="post">
+    Nama Mata Kuliah<input type="text" name="Nama_mata_kuliah"><br>
+    Kode Mata Kuliah<input type="text" name="Kode_mata_kuliah"><br>
+    sks<input type="text" name="sks"><br>
+    <input type="submit" Value="Tambah mata kuliah">
+</form>
